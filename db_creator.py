cc83462ccb2059b065d8cbbe36273b8a45a8391f
山村 晋作 <gretel@MR-CLIENT-17.local>
2019-08-23 02:58:16 +0900

    add 実装した

--- db_creator.py
+++ db_creator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import logging
+import re
 
 import jaconv
 from sqlalchemy.exc import IntegrityError
@@ -21,6 +22,60 @@ def main():
     db = Database(DB_PATH)
     dl = DictLoader(DICT_PATH)
 
+    # laod_dictionaly(db, dl)
+
+    initial = ""
+    length = -1
+    used = []
+
+    message = ""
+
+    pattern_hiragana = re.compile(r"^[ぁ-ん]$")
+    pattern_digit = re.compile(r"^\d+$")
+
+    while True:
+        command = input(f"{message}> ")
+
+        if len(command) == 0:
+            if length == -1:
+                continue
+
+        elif len(command) < 2:
+            message = f"Syntax error"
+            continue
+
+        else:
+            initial = jaconv.kata2hira(command[0])
+            length  = jaconv.z2h(command[1:], digit=True)
+
+            if not pattern_hiragana.match(initial):
+                message = "Syntax error"
+                continue
+
+            if not pattern_digit.match(length):
+                message = "Syntax error"
+                continue
+
+
+            message = f"{initial}:{length}"
+
+
+        for row in db.session.query(Dictionaly.kana, Dictionaly.length).filter(Dictionaly.kana.like(f"{initial}%")).filter_by(length=int(length)).group_by(Dictionaly.kana):
+            suggest = row[0]
+            if suggest in used:
+                continue
+
+            used.append(suggest)
+            break
+
+        print(f"{suggest}\n")
+
+    db.close()
+
+
+
+
+def laod_dictionaly(db, dl):
     for i, row in enumerate(dl.load(FROM_ROW, TO_ROW)):
         text = jaconv.kata2hira(row[11])
         db.session.add(Dictionaly(kana=text, length=len(text)))
@@ -33,9 +88,9 @@ def main():
 
             logging.info(f"commited: {i}")
 
-
     db.session.commit()
-    db.close()
+    
+    return
 
 
 
